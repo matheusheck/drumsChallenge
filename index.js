c40e460e381f871be44010d3c9d391425cd234a9
Matheus Heck <39709032+matheusheck@users.noreply.github.com>
2020-06-16 02:14:52 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+var numDrums = document.querySelectorAll('.drum').length;
+
+for (var i = 0; i < numDrums; i++) {
+  document.querySelectorAll('.drum')[i].addEventListener('click', function() {
+    alert('I got clicked!');
+  });
+}
